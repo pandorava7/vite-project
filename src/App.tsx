c4950f2ccd444440eab7edca7b8baf2c6e82d50d
pando <pando@PANDORA>
2025-10-27 13:09:24 +0800

    change file

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,6 @@ import { useEffect } from "react";
 
 const App: React.FC = () => {
 
-  // useEffect(() => {
-  //   // 页面加载完毕后检查 URL 的 hash
-  //   if (window.location.hash) {
-  //     const id = window.location.hash.replace("#", "");
-  //     const element = document.getElementById(id);
-  //     if (element) {
-  //       element.scrollIntoView({ behavior: "smooth" });
-  //     }
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   // 仅第一次进入网站时执行
-  //   if (!window.location.hash) {
-  //     window.location.hash = "#portfolio";
-  //   }
-  // }, []);
-
-  // 在 React 组件挂载后执行（例如在 App.jsx 或 useEffect 中）
-useEffect(() => {
-  if (window.location.hash) {
-    const id = window.location.hash.substring(1);
-    const el = document.getElementById(id);
-    if (el) {
-      const yOffset = -60; // 导航栏高度
-      const y = el.getBoundingClientRect().top + window.scrollY + yOffset;
-      window.scrollTo({ top: y, behavior: "auto" }); // 不需要平滑滚动
-    }
-  }
-}, []);
 
   return (
     <div className="app">
